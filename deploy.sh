3a9452d89124fb0c818c5e245aa43186ff92a66d
yalezhang <yalezhang.china@gmail.com>
2021-09-07 11:00:31 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -19,5 +19,5 @@ git status
 sleep 5
 git add .
 git commit -m "Update gh-pages"
-git remote add origin git@github.com:yalezhangk/blog.git
+git remote add origin git@github.com:yalezhangk/test.git
 git push -f "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages
